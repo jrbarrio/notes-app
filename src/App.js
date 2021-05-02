ec7c78f81abde1dd8861deea05371f1e776b85f8
jrbarrio <jorge.roldan@gmail.com>
2021-05-02 16:12:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Description from './components/Description';
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message color='red' message="Estamos trabajando en ello..."></Message>
+      <Description></Description>
     </div>
   );
 }
---
+++ src/components/Description.js
@@ -0,0 +1,5 @@
+const Description = () => {
+    return <p>Esta es la app del curso Fullstack Bootcamp</p>
+}
+
+export default Description;
